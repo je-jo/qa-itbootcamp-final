6f782c385896adec63e31080b8b9d4f9ccb63dd7
Jelena Jovicic <jejovicic@gmail.com>
2024-10-22 15:45:29 +0200

    Add some inventory page locators

--- src/test/java/Pages/InventoryPage.java
+++ src/test/java/Pages/InventoryPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class InventoryPage extends BaseTest {
 
     public InventoryPage() {
@@ -13,6 +15,18 @@ public class InventoryPage extends BaseTest {
 
     @FindBy(className = "app_logo")
     public WebElement header;
+
+    @FindBy(className = "inventory_item_name")
+    public List<WebElement> itemNames;
+
+    @FindBy(className = "inventory_item_desc")
+    public List<WebElement> itemDescriptions;
+
+    @FindBy(className = "inventory_item_price")
+    public List<WebElement> itemPrices;
+
+    @FindBy(css = "img[class='inventory_item_img']")
+    public List<WebElement> itemImages;
 
     // metode
 
